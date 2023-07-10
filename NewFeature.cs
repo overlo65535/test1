c83913b6edba3f86d34f91363d9f6db35524c270
aspb1 <aspb1@gmail.com>
2023-07-10 18:41:15 +0300

    Add Student

--- NewFeature.cs
+++ NewFeature.cs
@@ -3,4 +3,6 @@ namespace Name1
     public record Person(string name, string lastName, string jobTitle);
 
     public record Employee(string name, string lastName, string jobTitle);
+
+    public record Student(string name, string lastName, string facultyTitle);
 }
