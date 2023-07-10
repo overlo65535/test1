9fc8e610925175eef982ec535f4509b47f098188
aspb1 <aspb1@gmail.com>
2023-07-10 18:46:55 +0300

    Add Supervisor

--- NewFeature.cs
+++ NewFeature.cs
@@ -7,4 +7,6 @@ namespace Name1
     public record Student(string name, string lastName, string facultyTitle);
 
     public record Manager(string name, string lastName, double salary);
+
+    public record Supervisor(string name, string lastName, string nickname);
 }
